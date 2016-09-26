1dcac3174b5376f5c52cfe9931a5a951b0a5ab44
Hem Brahmbhatt <hamant.brahmbhatt@net-a-porter.com>
2016-09-26 13:11:08 +0100

    deleted default and large svg icon sets

--- config.js
+++ config.js
@@ -4,8 +4,6 @@ module.exports = {
     height: 10,
     width: 10,
     sprites: {
-      default: { width: 1, height: 1 },
-      large: { width: 1.5, height: 1.5 },
       social: { width: 2, height: 2 },
       myaccount: { width: 2, height: 2 }
     },
